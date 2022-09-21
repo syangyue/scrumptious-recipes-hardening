7fb4c5936a31cf217eded92f23448bab45efde39
yangyue <daisysong19900215@gmail.com>
2022-09-20 18:56:47 -0700

    Fixed a bug where rating a deleted recipe does not show an error

--- recipes/views.py
+++ recipes/views.py
@@ -7,16 +7,18 @@ from django.views.generic.list import ListView
 from recipes.forms import RatingForm
 from recipes.forms import RecipeForm
 from recipes.models import Recipe
-\
 
 
 def log_rating(request, recipe_id):
     if request.method == "POST":
         form = RatingForm(request.POST)
         if form.is_valid():
-            rating = form.save(commit=False)
-            rating.recipe = Recipe.objects.get(pk=recipe_id)
-            rating.save()
+            try:
+                rating = form.save(commit=False)
+                rating.recipe = Recipe.objects.get(pk=recipe_id)
+                rating.save()
+            except Recipe.DoesNotExist:
+                return redirect("recipes_list")
     return redirect("recipe_detail", pk=recipe_id)
 
 
