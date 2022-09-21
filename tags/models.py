4d879cadf02429cc84b1d8d7a4f0f36cfd099917
yangyue <daisysong19900215@gmail.com>
2022-09-20 21:10:01 -0700

    Refactored the templates to harden the app

--- tags/models.py
+++ tags/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=20)
     recipes = models.ManyToManyField("recipes.Recipe", related_name="tags")
+
+    def __str__(self):
+        return self.name
